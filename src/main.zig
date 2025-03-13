c0302fb029cc83c0b0856ab604e5f317a1b3cc0e
nronzel <86695181+nronzel@users.noreply.github.com>
2025-03-13 15:18:24 -0400

    update to zig 0.14

--- src/main.zig
+++ src/main.zig
@@ -5,31 +5,26 @@ const expect = std.testing.expect;
 const expectEqualStrings = std.testing.expectEqualStrings;
 const expectEqual = std.testing.expectEqual;
 const expectError = std.testing.expectError;
+const assert = std.debug.assert;
 
 pub fn main() !void {
-    // initialize GPA for allocations
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    // process and allocate args
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
-    // initialize a buffered writer to stdout
     var buf_writer = std.io.bufferedWriter(std.io.getStdOut().writer());
     const stdout = buf_writer.writer();
 
-    // check for flag and print help message
     if (args.len > 1 and (mem.eql(u8, args[1], "-h") or mem.eql(u8, args[1], "--help"))) {
         try printUsage(stdout);
         return;
     }
 
-    // parse args
     const parsed_args = try ParseArgs(args);
 
-    // get and allocate the absolute path
     const absolute_path = try getAbsolutePath(allocator, parsed_args.dir_path);
     defer allocator.free(absolute_path);
 
@@ -55,10 +50,10 @@ pub fn main() !void {
 // Counts keeps track of the number of directories, files, symlinks, and others.
 // other is defined below.
 const Counts = struct {
-    dirs: usize,
-    files: usize,
-    sym_links: usize,
-    other: usize,
+    dirs: u16,
+    files: u16,
+    sym_links: u16,
+    other: u16,
 };
 
 // only tracking files, dirs, and sym-links. "other" represents the following:
@@ -90,8 +85,8 @@ const Entry = struct {
 fn printDirectory(
     allocator: mem.Allocator,
     path: []const u8,
-    max_depth: usize,
-    current_depth: usize,
+    max_depth: u4,
+    current_depth: u4,
     writer: anytype,
     counts: *Counts,
 ) !void {
@@ -163,28 +158,35 @@ fn printDirectory(
 }
 
 const ParsedArgs = struct {
-    max_depth: usize,
     dir_path: []const u8,
+    max_depth: u4,
 };
 
 fn ParseArgs(args: []const []const u8) !ParsedArgs {
     var result = ParsedArgs{
-        .max_depth = 2,
         .dir_path = ".",
+        .max_depth = 2,
     };
 
     // if only one arg is supplied, try to parse it as an int for the max_depth.
     // if this fails, assume that a directory was supplied instead.
     if (args.len == 2) {
-        if (std.fmt.parseInt(usize, args[1], 10)) |depth| {
+        if (std.fmt.parseInt(u4, args[1], 10)) |depth| {
+            assert(depth >= 0);
             result.max_depth = depth;
         } else |_| {
+            assert(!std.mem.eql(u8, args[1], ""));
             result.dir_path = args[1];
         }
     } else if (args.len == 3) {
         result.dir_path = args[1];
-        result.max_depth = try std.fmt.parseInt(usize, args[2], 10);
+        result.max_depth = try std.fmt.parseInt(u4, args[2], 10);
     }
+
+    if (result.max_depth > 10) {
+        result.max_depth = 10;
+    }
+
     return result;
 }
 
@@ -203,7 +205,7 @@ fn entryLessThan(context: void, a: Entry, b: Entry) bool {
     return std.ascii.lessThanIgnoreCase(n1, n2);
 }
 
-fn printIndentation(level: usize, writer: anytype) !void {
+fn printIndentation(level: u8, writer: anytype) !void {
     for (0..level) |_| {
         try writer.writeAll("    ");
     }
@@ -246,7 +248,7 @@ test "ParseArgs" {
     {
         const args = [_][]const u8{"program"};
         const result = try ParseArgs(&args);
-        try expectEqual(@as(usize, 2), result.max_depth);
+        try expectEqual(@as(u4, 2), result.max_depth);
         try expectEqualStrings(".", result.dir_path);
     }
 
@@ -254,7 +256,7 @@ test "ParseArgs" {
     {
         const args = [_][]const u8{ "program", "5" };
         const result = try ParseArgs(&args);
-        try expectEqual(@as(usize, 5), result.max_depth);
+        try expectEqual(@as(u4, 5), result.max_depth);
         try expectEqualStrings(".", result.dir_path);
     }
 
@@ -262,7 +264,7 @@ test "ParseArgs" {
     {
         const args = [_][]const u8{ "program", "/home/user" };
         const result = try ParseArgs(&args);
-        try expectEqual(@as(usize, 2), result.max_depth);
+        try expectEqual(@as(u4, 2), result.max_depth);
         try expectEqualStrings("/home/user", result.dir_path);
     }
 
@@ -270,7 +272,7 @@ test "ParseArgs" {
     {
         const args = [_][]const u8{ "program", "/home/user", "3" };
         const result = try ParseArgs(&args);
-        try expectEqual(@as(usize, 3), result.max_depth);
+        try expectEqual(@as(u4, 3), result.max_depth);
         try expectEqualStrings("/home/user", result.dir_path);
     }
 
@@ -321,8 +323,8 @@ test "printDirectory" {
     try expect(std.mem.indexOf(u8, output, "📄 file3.txt") != null);
 
     // Check the counts
-    try expectEqual(@as(usize, 3), counts.dirs);
-    try expectEqual(@as(usize, 3), counts.files);
-    try expectEqual(@as(usize, 0), counts.sym_links);
-    try expectEqual(@as(usize, 0), counts.other);
+    try expectEqual(@as(u16, 3), counts.dirs);
+    try expectEqual(@as(u16, 3), counts.files);
+    try expectEqual(@as(u16, 0), counts.sym_links);
+    try expectEqual(@as(u16, 0), counts.other);
 }
